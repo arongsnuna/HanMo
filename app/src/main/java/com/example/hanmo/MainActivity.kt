39550ac4491c5d8728d5850a252e93f9f024346a
Jin0-K <jykim0827@gamil.com>
2022-07-23 19:21:53 +0900

    Button List with Recycler View

--- app/src/main/java/com/example/hanmo/MainActivity.kt
+++ app/src/main/java/com/example/hanmo/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.hanmo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var parknameAdaptor: ParknameAdaptor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        parknameAdaptor = ParknameAdaptor(mutableListOf())
+
+        parkList_lv.adapter = parknameAdaptor
+        // layout manager
+        parkList_lv.layoutManager = LinearLayoutManager(this)
+
     }
 }
